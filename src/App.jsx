baeba5f7d4dd243f79f4501beb5a47559f2b4a0b
Andres Gallego <andresfelipegallegozuleta@gmail.com>
2024-11-11 20:02:44 -0500

    refactor layout and styling: integrate Tailwind CSS, update HTML structure, and remove unused CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import "./App.css";
 
+import Header from "./components/Header";
+import ListCountries from "./components/ListCountries";
 import { useGetCountriesQuery } from "./services/countries";
 
 function App() {
-  const { data, error, isLoading } = useGetCountriesQuery();
+  // const { data, error, isLoading } = useGetCountriesQuery();
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
+  // if (isLoading) return <div>Loading...</div>;
+  // if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <div>
-      {data.map((country) => (
-        <div key={country.name.common}>{country.name.common}</div>
-      ))}
-    </div>
+    <main className="min-h-100vh w-full flex flex-col justify-start items-center font-nunito-sans">
+      <Header />
+      <section className="">
+        <ListCountries countries={[]} />
+      </section>
+    </main>
   );
 }
 
