7e14ea31204432b67175f6cc527f0d54eef54ead
Andres Gallego <andresfelipegallegozuleta@gmail.com>
2024-11-15 22:54:48 -0500

    add routing functionality: integrate react-router-dom for navigation;

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
 import "./App.css";
 
+import { Route, Routes } from "react-router-dom";
+
+import CountryDetails from "./components/CountryDetails";
 import Header from "./components/Header";
 import ListCountries from "./components/ListCountries";
 import ManageCountries from "./components/ManageCountries";
 import { useGetCountriesQuery } from "./services/countries";
 
 function App() {
-  // const { data, error, isLoading } = useGetCountriesQuery();
+  const { data, error, isLoading } = useGetCountriesQuery();
 
-  // if (isLoading) return <div>Loading...</div>;
-  // if (error) return <div>Error: {error.message}</div>;
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
 
   return (
     <main className="min-h-100vh w-full flex flex-col justify-start items-center font-nunito-sans">
       <Header />
       <section className="max-w-1440 w-full flex justify-center flex-col pl-11 pr-11 xl:pl-2 xl:pr-2">
         <ManageCountries />
-        <ListCountries countries={[]} />
+        <Routes>
+          <Route path="/" element={<ListCountries countries={data} />} />
+          <Route path="/country/:name" element={<CountryDetails />} />
+        </Routes>
       </section>
     </main>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import "./index.css";
 
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
@@ -9,7 +10,9 @@ import { store } from "./store/store.js";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
